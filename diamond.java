2daa8dc1f1d538856abc5788439ec51b344086ec
prannnjal <113020303+prannnjal@users.noreply.github.com>
2022-09-14 15:49:51 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,33 @@
+public class diamond {
+
+    public static void main(String[] args) {
+
+        // set a size
+        int size = 5;
+        // upper pyramid
+        for (int i = 1; i <= size; i++) {
+            // print spaces
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            // print star
+            for (int k = 0; k < i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower pyramid
+        for (int i = 1; i <= size - 1; i++) {
+            // print spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // print star
+            for (int k = (size - i) * 2 - 1; k > 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
